a4c4faf398eeca0ea4343bfed08a95bc2ea61fb7
裴振庭 <1612750269@qq.com>
2023-06-06 17:31:17 +0800

    创建文件夹

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 let path = require('path');
 
-let datDir = 'D:\\文件\\微信\\WeChat Files\\wxid_7iu4zm2yl3zq12\\FileStorage\\Image\\2022-03';
+let datDir = 'd:/2022-05';
 
 exports.DatDir = path.join(datDir)
 exports.OutDir = 'img';
